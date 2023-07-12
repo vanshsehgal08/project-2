7a64df11c1eca7a1b78124c2a4706ec7cb51a191
Vansh Sehgal <vanshsehgal2019@gmail.com>
2023-07-12 15:42:26 +0530

    jj
    jj

---
+++ 5) Projects/3) India-Cultural-Site/demo/india/settings.js
@@ -0,0 +1,324 @@
+var map_cfg = {
+ "mapWidth": "100%",
+ "mapHeight": 500,
+ "shadowWidth": 2,
+ "shadowOpacity": 0.2,
+ "shadowColor": "black",
+ "shadowX": 0,
+ "shadowY": 0,
+ "iPhoneLink": true,
+ "isNewWindow": false,
+ "zoomEnable": false,
+ "zoomEnableControls": false,
+ "zoomIgnoreMouseScroll": false,
+ "zoomMax": 8,
+ "zoomStep": 0.8,
+ "borderColor": "#ffffff",
+ "borderColorOver": "#ffffff",
+ "borderOpacity": 0.5,
+ "nameColor": "#ffffff",
+ "nameColorOver": "#ffffff",
+ "nameFontSize": "10px",
+ "nameFontWeight": "bold",
+ "nameStroke": true,
+ "nameStrokeColor": "#000000",
+ "nameStrokeColorOver": "#000000",
+ "nameStrokeWidth": 1.5,
+ "nameStrokeOpacity": 0.5,
+ "overDelay": 300,
+ "nameAutoSize": false,
+ "tooltipOnHighlightIn": false,
+ "freezeTooltipOnClick": false,
+ "mapId": "7f5w8",
+ "map_data": {
+  "st1": {
+   "name": "Andaman and Nicobar Islands",
+   "shortname": "AN",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st2": {
+   "name": "Andhra Pradesh",
+   "shortname": "AP",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st3": {
+   "name": "Arunachal Pradesh",
+   "shortname": "AR",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st4": {
+   "name": "Assam",
+   "shortname": "AS",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st5": {
+   "name": "Bihar",
+   "shortname": "BR",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st6": {
+   "name": "Chandigarh",
+   "shortname": "CH",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st7": {
+   "name": "Chhattisgarh",
+   "shortname": "CT",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st8": {
+   "name": "Dadra and Nagar Haveli and Daman and Diu",
+   "shortname": "DD",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st9": {
+   "name": "National Capital Territory of Delhi",
+   "shortname": "DL",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st10": {
+   "name": "Goa",
+   "shortname": "GA",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st11": {
+   "name": "Gujarat",
+   "shortname": "GJ",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st12": {
+   "name": "Himachal Pradesh",
+   "shortname": "HP",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st13": {
+   "name": "Haryana",
+   "shortname": "HR",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st14": {
+   "name": "Jharkhand",
+   "shortname": "JH",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st15": {
+   "name": "Jammu and Kashmir",
+   "shortname": "JK",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st16": {
+   "name": "Karnataka",
+   "shortname": "KA",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st17": {
+   "name": "Kerala",
+   "shortname": "KL",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st18": {
+   "name": "Ladakh",
+   "shortname": "LA",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st19": {
+   "name": "Lakshadweep",
+   "shortname": "LD",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st20": {
+   "name": "Maharashtra",
+   "shortname": "MH",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st21": {
+   "name": "Meghalaya",
+   "shortname": "ML",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st22": {
+   "name": "Manipur",
+   "shortname": "MN",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st23": {
+   "name": "Madhya Pradesh",
+   "shortname": "MP",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st24": {
+   "name": "Mizoram",
+   "shortname": "MZ",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st25": {
+   "name": "Nagaland",
+   "shortname": "NL",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st26": {
+   "name": "Orissa",
+   "shortname": "OR",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st27": {
+   "name": "Punjab",
+   "shortname": "PB",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st28": {
+   "name": "Puducherry",
+   "shortname": "PY",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st29": {
+   "name": "Rajasthan",
+   "shortname": "RJ",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st30": {
+   "name": "Sikkim",
+   "shortname": "SK",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st31": {
+   "name": "Telangana",
+   "shortname": "TG",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st32": {
+   "name": "Tamil Nadu",
+   "shortname": "TN",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st33": {
+   "name": "Tripura",
+   "shortname": "TR",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st34": {
+   "name": "Uttar Pradesh",
+   "shortname": "UP",
+   "comment": "hellloo",
+   "link": "./states/uttar_pradesh.html",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st35": {
+   "name": "Uttarakhand",
+   "shortname": "UT",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  },
+  "st36": {
+   "name": "West Bengal",
+   "shortname": "WB",
+   "comment": "",
+   "link": "",
+   "color": "#7798bb",
+   "colorOver": "#366CA3"
+  }
+ },
+ "points": {}
+};
---
+++ 5) Projects/3) India-Cultural-Site/india/settings.js
@@ -0,0 +1,324 @@
+var map_cfg = {
+ "mapWidth": "100%",
+ "mapHeight": 500,
+ "shadowWidth": 2,
+ "shadowOpacity": 0.2,
+ "shadowColor": "black",
+ "shadowX": 0,
+ "shadowY": 0,
+ "iPhoneLink": true,
+ "isNewWindow": false,
+ "zoomEnable": false,
+ "zoomEnableControls": false,
+ "zoomIgnoreMouseScroll": false,
+ "zoomMax": 8,
+ "zoomStep": 0.8,
+ "borderColor": "#ffffff",
+ "borderColorOver": "#ffffff",
+ "borderOpacity": 0.5,
+ "nameColor": "#ffffff",
+ "nameColorOver": "#ffffff",
+ "nameFontSize": "10px",
+ "nameFontWeight": "bold",
+ "nameStroke": true,
+ "nameStrokeColor": "#000000",
+ "nameStrokeColorOver": "#000000",
+ "nameStrokeWidth": 1.5,
+ "nameStrokeOpacity": 0.5,
+ "overDelay": 300,
+ "nameAutoSize": false,
+ "tooltipOnHighlightIn": false,
+ "freezeTooltipOnClick": false,
+ "mapId": "7f5w8",
+ "map_data": {
+  "st1": {
+   "name": "Andaman and Nicobar Islands",
+   "shortname": "AN",
+   "comment": "",
+   "link": "./states/AN.html",
+   "color": "#2478ff",
+   "colorOver": "#1e61cc"
+  },
+  "st2": {
+   "name": "Andhra Pradesh",
+   "shortname": "AP",
+   "comment": "",
+   "link": "./states/AP.html",
+   "color": "#4cbd0f",
+   "colorOver": "#46b00c"
+  },
+  "st3": {
+   "name": "Arunachal Pradesh",
+   "shortname": "AR",
+   "comment": "",
+   "link": "./states/AR.html",
+   "color": "#fd853f",
+   "colorOver": "#ff7b2e"
+  },
+  "st4": {
+   "name": "Assam",
+   "shortname": "AS",
+   "comment": "",
+   "link": "./states/AS.html",
+   "color": "#fd853f",
+   "colorOver": "#ff7b2e"
+  },
+  "st5": {
+   "name": "Bihar",
+   "shortname": "BR",
+   "comment": "",
+   "link": "./states/BR.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st6": {
+   "name": "Chandigarh",
+   "shortname": "CH",
+   "comment": "",
+   "link": "./states/CH.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st7": {
+   "name": "Chhattisgarh",
+   "shortname": "CT",
+   "comment": "",
+   "link": "./states/CT.html",
+   "color": "#f0e6e6",
+   "colorOver": "#ddd8d4"
+  },
+  "st8": {
+   "name": "Dadra and Nagar Haveli and Daman and Diu",
+   "shortname": "DD",
+   "comment": "",
+   "link": "./states/DD.html",
+   "color": "#4cbd0f",
+   "colorOver": "#46b00c"
+  },
+  "st9": {
+   "name": "National Capital Territory of Delhi",
+   "shortname": "DL",
+   "comment": "",
+   "link": "./states/DL.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st10": {
+   "name": "Goa",
+   "shortname": "GA",
+   "comment": "",
+   "link": "./states/GA.html",
+   "color": "#4cbd0f",
+   "colorOver": "#46b00c"
+  },
+  "st11": {
+   "name": "Gujarat",
+   "shortname": "GJ",
+   "comment": "",
+   "link": "./states/GJ.html",
+   "color": "#f0e6e6",
+   "colorOver": "#ddd8d4"
+  },
+  "st12": {
+   "name": "Himachal Pradesh",
+   "shortname": "HP",
+   "comment": "",
+   "link": "./states/HP.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st13": {
+   "name": "Haryana",
+   "shortname": "HR",
+   "comment": "",
+   "link": "./states/HR.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st14": {
+   "name": "Jharkhand",
+   "shortname": "JH",
+   "comment": "",
+   "link": "./states/JH.html",
+   "color": "#f0e6e6",
+   "colorOver": "#ddd8d4"
+  },
+  "st15": {
+   "name": "Jammu and Kashmir",
+   "shortname": "JK",
+   "comment": "",
+   "link": "./states/JK.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st16": {
+   "name": "Karnataka",
+   "shortname": "KA",
+   "comment": "",
+   "link": "./states/KA.html",
+   "color": "#4cbd0f",
+   "colorOver": "#46b00c"
+  },
+  "st17": {
+   "name": "Kerala",
+   "shortname": "KL",
+   "comment": "",
+   "link": "./states/KL.html",
+   "color": "#4cbd0f",
+   "colorOver": "#46b00c"
+  },
+  "st18": {
+   "name": "Ladakh",
+   "shortname": "LA",
+   "comment": "",
+   "link": "./states/LA.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st19": {
+   "name": "Lakshadweep",
+   "shortname": "LD",
+   "comment": "",
+   "link": "./states/LD.html",
+   "color": "#2478ff",
+   "colorOver": "#1e61cc"
+  },
+  "st20": {
+   "name": "Maharashtra",
+   "shortname": "MH",
+   "comment": "",
+   "link": "./states/MH.html",
+   "color": "#4cbd0f",
+   "colorOver": "#46b00c"
+  },
+  "st21": {
+   "name": "Meghalaya",
+   "shortname": "ML",
+   "comment": "",
+   "link": "./states/ML.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st22": {
+   "name": "Manipur",
+   "shortname": "MN",
+   "comment": "",
+   "link": "./states/MN.html",
+   "color": "#fd853f",
+   "colorOver": "#ff7b2e"
+  },
+  "st23": {
+   "name": "Madhya Pradesh",
+   "shortname": "MP",
+   "comment": "",
+   "link": "./states/MP.html",
+   "color": "#f0e6e6",
+   "colorOver": "#ddd8d4"
+  },
+  "st24": {
+   "name": "Mizoram",
+   "shortname": "MZ",
+   "comment": "",
+   "link": "./states/MZ.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st25": {
+   "name": "Nagaland",
+   "shortname": "NL",
+   "comment": "",
+   "link": "./states/NL.html",
+   "color": "#fd853f",
+   "colorOver": "#ff7b2e"
+  },
+  "st26": {
+   "name": "Orissa",
+   "shortname": "OR",
+   "comment": "",
+   "link": "./states/OR.html",
+   "color": "#4cbd0f",
+   "colorOver": "#46b00c"
+  },
+  "st27": {
+   "name": "Punjab",
+   "shortname": "PB",
+   "comment": "",
+   "link": "./states/PB.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st28": {
+   "name": "Puducherry",
+   "shortname": "PY",
+   "comment": "",
+   "link": "./states/PY.html",
+   "color": "#4cbd0f",
+   "colorOver": "#46b00c"
+  },
+  "st29": {
+   "name": "Rajasthan",
+   "shortname": "RJ",
+   "comment": "",
+   "link": "./states/RJ.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st30": {
+   "name": "Sikkim",
+   "shortname": "SK",
+   "comment": "",
+   "link": "./states/SK.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st31": {
+   "name": "Telangana",
+   "shortname": "TG",
+   "comment": "",
+   "link": "./states/TG.html",
+   "color": "#4cbd0f",
+   "colorOver": "#46b00c"
+  },
+  "st32": {
+   "name": "Tamil Nadu",
+   "shortname": "TN",
+   "comment": "",
+   "link": "./states/TN.html",
+   "color": "#4cbd0f",
+   "colorOver": "#46b00c"
+  },
+  "st33": {
+   "name": "Tripura",
+   "shortname": "TR",
+   "comment": "",
+   "link": "./states/TR.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st34": {
+   "name": "Uttar Pradesh",
+   "shortname": "UP",
+   "comment": "",
+   "link": "./states/UP.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st35": {
+   "name": "Uttarakhand",
+   "shortname": "UT",
+   "comment": "",
+   "link": "./states/UK.html",
+   "color": "#ff7424",
+   "colorOver": "#e86a21"
+  },
+  "st36": {
+   "name": "West Bengal",
+   "shortname": "WB",
+   "comment": "",
+   "link": "./states/WB.html",
+   "color": "#f0e6e6",
+   "colorOver": "#ddd8d4"
+  }
+ },
+ "points": {}
+};
